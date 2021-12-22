deded219835a8068140cea79df07400cd9a142c0
rylandmdufour <95839434+rylandmdufour@users.noreply.github.com>
2021-12-22 15:32:05 -0500

    Create node_exporter_install.sh

---
+++ node_exporter_install.sh
@@ -0,0 +1,33 @@
+#/bin/bash
+
+wget https://github.com/prometheus/node_exporter/releases/download/v1.3.1/node_exporter-1.3.1.linux-amd64.tar.gz
+
+tar xfz node*.tar.gz  
+rm node*.tar.gz  
+mv node_exporter-1.3.1.linux-amd64/node_exporter /usr/local/bin/
+rm -rf node_exporter*
+
+
+cat << EOF > /etc/systemd/system/node_exporter.service 
+[Unit]
+Description=Node Exporter
+Wants=network-online.target
+After=network-online.target
+
+[Service]
+User=node_exporter
+Group=node_exporter
+Type=simple
+ExecStart=/usr/local/bin/node_exporter
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+
+useradd --no-create-home --shell /bin/false node_exporter
+chown node_exporter:node_exporter /usr/local/bin/node_exporter
+systemctl daemon-reload
+systemctl enable node_exporter
+systemctl start node_exporter
+systemctl status node_exporter
